dfe51aa8e98a4ff49fbcd56b4c24a8379a529331
Asel Peiris <31003923+AuraOfDivinity@users.noreply.github.com>
2018-12-21 16:22:34 +0530

    Change1
    New css based changes.

--- src/Content.js
+++ src/Content.js
@@ -4,28 +4,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 class Content extends React.Component {
   render() {
+    const styling = {
+      padding: "50dp"
+    };
+
+    const CSSDemoStyle = {
+      margin: "5%",
+      width: "40%"
+    };
+
     return (
       <Row>
-        <Col sm="6">
-          <Card body>
-            <CardTitle>Special Title Treatment</CardTitle>
-            <CardText>
-              With supporting text below as a natural lead-in to additional
-              content.
-            </CardText>
-            <Button>Go somewhere</Button>
-          </Card>
-        </Col>
-        <Col sm="6">
-          <Card body>
-            <CardTitle>Special Title Treatment</CardTitle>
-            <CardText>
-              With supporting text below as a natural lead-in to additional
-              content.
-            </CardText>
-            <Button>Go somewhere</Button>
-          </Card>
-        </Col>
+        <div style={CSSDemoStyle}>
+          <Col sm="6">
+            <Card body>
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>
+                s With supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Button>Go somewhere</Button>
+            </Card>
+          </Col>
+        </div>
+
+        <div style={CSSDemoStyle}>
+          <Col sm="6">
+            <Card body>
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Button>Go somewhere</Button>
+            </Card>
+          </Col>
+        </div>
       </Row>
     );
   }
